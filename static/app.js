4bba32ae4f01f14d836925c1464d9345f2d67e67
tchikyy <76624013+tchikyy@users.noreply.github.com>
2021-09-02 06:59:57 +0200

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,9 +1,13 @@
-let fadeElement = [...document.querySelectorAll('.fade-up')]
+let fadeElement = [...document.querySelectorAll('.fade-up')];
+let fadeIn = [...document.querySelectorAll('.fade-in')];
+let fadeIn2 = [...document.querySelectorAll('.fade-in2')];
 
 let options = {
     rootMargin: '-10%',
     threshold: 0.0
 }
+
+/* --------------text's transition---------------- */
 
 let observer = new IntersectionObserver(showItem, options);
 
@@ -18,3 +22,56 @@ function showItem(entries){
 fadeElement.forEach(item => {
     observer.observe(item);
 })
+
+/* --------------flask's transition--------------- */
+
+let observer2 = new IntersectionObserver(showItem2, options);
+
+function showItem2(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('in');
+        }
+    })
+}
+
+fadeIn.forEach(item => {
+    observer2.observe(item);
+})
+
+
+/* --------------html's transition---------------- */
+
+let observer3 = new IntersectionObserver(showItem3, options);
+
+function showItem3(entries){
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('in2');
+        }
+    })
+}
+
+fadeIn2.forEach(item => {
+    observer3.observe(item);
+})
+
+/* --------------question's movements----------------------- */
+
+function onClick(id){
+    let element = document.getElementById(id);
+    let childes = element.childNodes;
+    childes[1].classList.toggle("turn");
+
+    let sibling = element.nextElementSibling;
+
+    if( sibling.style.height == ""){
+        sibling.style.height = "fit-content";
+        sibling.style.padding = "10px 20px";
+        /* sibling.style.transition = "height 3s"; */
+    }else{
+        sibling.style.height = "";
+        sibling.style.padding = "";
+    }
+
+}
